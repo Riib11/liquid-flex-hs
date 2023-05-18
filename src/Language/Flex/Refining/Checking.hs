59181695dc4fc258b37d3f56b374dc0b1e609bae
riib11 <henry@monkeyface.com>
2023-05-18 22:17:15 +0800

    removed debug

--- src/Language/Flex/Refining/Checking.hs
+++ src/Language/Flex/Refining/Checking.hs
@@ -566,7 +566,6 @@ introConstants :: StateT CheckingCtx RefiningM ()
 introConstants = do
   -- intro constant as equation
   asks (^. ctxConstants . to Map.toList) >>= traverse_ \(tmId, tm) -> do
-    liftIO . putStrLn $ "[introConstants] intro constant:\n  - tmId = " <> prettyShow tmId <> "\n  - tm = " <> prettyShow tm
     let sym = makeTermIdSymbol tmId
     tm' <- lift $ transTerm tm
     ex <- reflTermState tm'
